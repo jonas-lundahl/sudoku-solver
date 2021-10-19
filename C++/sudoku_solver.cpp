8e095b5d18ab3c643438105d3b6d2d25c08362c5
Jonas Lundahl <jonas.lundahl@outlook.com>
2021-10-19 12:57:52 +0200

    Working C++ Sudoku Solver

--- C++/sudoku_solver.cpp
+++ C++/sudoku_solver.cpp
@@ -1,7 +1,90 @@
 #include "sudoku_solver.h"
 
+#include "bits/stdc++.h"
+
+using namespace std;
+
+// Auxiliary print function for debugging
+ostream& operator<<(ostream& os, const board_t& board) {
+    for (size_t row = 0; row < 9; row++) {
+        for (size_t col = 0; col < 9; col++) {
+            os << board[row][col] << " ";
+        }
+        os << "\n";
+    }
+    return os;
+}
+
 void SudokuSolver::setBoard(board_t b) { board = b; }
 
-bool SudokuSolver::solveBoard() { return true; }
-
 board_t& SudokuSolver::getBoard() { return board; }
+
+bool SudokuSolver::solveBoard() {
+    fixed = {};
+    for (size_t row = 0; row < 9; row++) {
+        for (size_t col = 0; col < 9; col++) {
+            fixed[row][col] = board[row][col] > 0 ? 2 : 0;
+        }
+    }
+
+    return solve(0, 0);
+}
+
+bool SudokuSolver::solve(int row, int col) {
+    if (row == 9) return true;  // end of board reached
+
+    int next_row, next_col;
+    if (col == 8) {
+        next_row = row + 1;
+        next_col = 0;
+    } else {
+        next_row = row;
+        next_col = col + 1;
+    }
+
+    if (fixed[row][col] == 2)
+        return solve(next_row, next_col);  // already occupied
+
+    // Test all possibilities
+    for (int candidate = 1; candidate <= 9; candidate++) {
+        int d;
+
+        // Check rows
+        for (d = 0; d < 9; ++d) {
+            if (board[d][col] == candidate) break;
+        }
+        if (d < 9) continue;
+
+        // Check cols
+        for (d = 0; d < 9; ++d) {
+            if (board[row][d] == candidate) break;
+        }
+        if (d < 9) continue;
+
+        // Check box
+        int brow, bcol;  // top left corner of current box
+        brow = row / 3 * 3;
+        bcol = col / 3 * 3;
+        d = 0;
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[brow + r][bcol + c] == candidate) break;
+                d++;
+            }
+        }
+        if (d < 9) continue;
+
+        // Candidate can be placed
+        fixed[row][col] = 1;
+        board[row][col] = candidate;
+
+        if (solve(next_row, next_col)) {
+            return true;
+        } else {  // solution failed, restore board
+            fixed[row][col] = 0;
+            board[row][col] = 0;
+        }
+    }
+
+    return false;
+}
--- C++/sudoku_solver.h
+++ C++/sudoku_solver.h
@@ -1,15 +1,45 @@
 #pragma once
 
 #include <array>
+#include <iostream>
 
 typedef std::array<std::array<int, 9>, 9> board_t;
 
 class SudokuSolver {
    private:
+    /**
+     * Represents the Sudoku board that is to be solved.
+     */
     board_t board;
 
+    /**
+     * Keeps track of fixed and temporary numbers on the board. Fixed and
+     * temporary numbers are denoted by 2 and 1, respectively. Empty cells are
+     * 0.
+     */
+    board_t fixed;
+    /**
+     * Solves the Sudoku board starting from (row, col).
+     * @return true if solution was found, otherwise false
+     */
+    bool solve(int, int);
+
    public:
+    /**
+     * Sets the Sudoku board that is to be solved.
+     * @param board_t the board to solve
+     */
     void setBoard(board_t);
+
+    /**
+     * Get the Sudoku board contained in this solver.
+     * @return the Sudoku board
+     */
     board_t& getBoard();
+
+    /**
+     * Solves the Sudoku board. Returns true if solution was found, otherwise
+     * false.
+     */
     bool solveBoard();
 };
